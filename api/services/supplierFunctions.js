b4b6e6b6aea4846f75d37e8454895daf88665014
Will Ricketts <will@willricketts.com>
2014-08-12 11:56:00 -0400

    added findBySiblingId for supplierFunctions

--- api/services/supplierFunctions.js
+++ api/services/supplierFunctions.js
@@ -1,7 +1,19 @@
 module.exports = {
 
   findBySiblingId: function(buyerId, callback) {
-
+    Buyer.findOne({ id: buyerId }, function(err, buyer) {
+      errorHandler.serverError(err, res);
+      errorHandler.nullCollection(buyer, res);
+      Company.findOne({ id: buyer.company }, function(err, company) {
+        errorHandler.serverError(err, res);
+        errorHandler.nullCollection(company, res);
+        Supplier.findOne({ company: company.id }, function(err, supplier) {
+          errorHandler.serverError(err, res);
+          errorHandler.nullCollection(supplier, res);
+          callback(supplier);
+        });
+      });
+    });
   },
 
   findByCompanyId: function(companyId, callback) {
