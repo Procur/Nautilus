f7dccd7ca91a9c0c33fbedbc5dac86b83f785ee6
Will Ricketts <will@willricketts.com>
2014-08-13 13:34:25 -0400

    added index, show, and destroy actions for Log Controller

--- api/controllers/LogController.js
+++ api/controllers/LogController.js
@@ -8,15 +8,31 @@
 module.exports = {
 
   index: function(req, res) {
-
+    Log.find()
+        .then(function(logs) {
+          errorHandler.nullCollection(logs, res);
+          res.json(200, logs);
+        }).fail(function(err) {
+          errorHandler.serverError(err, res);
+        });
   },
 
   show: function(req, res) {
+    var p = req.params.all();
 
+    Log.findOne({ id: p.id }, function(err, log) {
+      errorHandler.qc(err, res, log);
+      res.json(200, log);
+    });
   },
 
   destroy: function(req, res) {
+    var p = req.params.all();
 
+    Log.destroy({ id: p.id }, function(err) {
+      errorHandler.serverError(err, res);
+      res.send(200, 'Log ' + p.id + ' deleted.');
+    });
   }
 };
 
