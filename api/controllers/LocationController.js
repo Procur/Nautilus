f9f3575f2c64b175fcfff71ed68fcfc1fc9a646e
Will Ricketts <will@willricketts.com>
2014-08-15 10:29:07 -0400

    refactored Location controller

--- api/controllers/LocationController.js
+++ api/controllers/LocationController.js
@@ -7,53 +7,83 @@
 
 module.exports = {
 
-  create: function(req, res) {
-    var p = req.params.all();
+  create: create,
+  index: index,
+  show: show,
+  modify: modify,
+  deactivate: deactivate
 
-    Location.create(p, function(err, location){
-      errorHandler.qc(err, res, location);
-      res.json(201, location);
-    });
-  },
-
-  index: function(req, res) {
-    Location.find()
-        .then(function(locations) {
-          errorHandler.nullCollection(locations, res);
-          res.json(200, locations);
-        }).fail(function(err) {
-          errorHandler.serverError(err, res);
-        });
-  },
-
-  show: function(req, res) {
-    var p = req.params.all();
-
-    Location.findOne({ id: p.id }, function(err, location){
-      errorHandler.qc(err, res, location);
-      res.json(200, location);
-    });
-  },
-
-  modify: function(req, res) {
-    var p = req.params.all;
-
-    Location.findOne({ id: p.id }, function(err, location) {
-      errorHandler.qc(err, res, location);
-      Location.update(p, function(err, location){
-        errorHandler.qc(err, res, location);
-        res.json(200, location);
-      });
-    });
-  },
-
-  destroy: function(req, res) {
-    var p = req.params.all();
-
-    Location.update({ id: p.id }, { active: false }, function(err, location){
-      errorHandler.qc(err, res, location);
-      res.send(200, 'Location ' + location.id + ' deactivated.');
-    });
-  }
 };
 
+function create(req, res) {
+  var p = req.params.all();
+
+  Location.create(p, function(err, location){
+    errorHandler.qc(err, res, location);
+    res.json(201, location);
+  });
+}
+
+function index(req, res) {
+  Location.find()
+      .then(function(locations) {
+        errorHandler.nullCollection(locations, res);
+        res.json(200, locations);
+      }).fail(function(err) {
+        errorHandler.serverError(err, res);
+      });
+}
+
+function show(req, res) {
+  var p = req.params.all();
+
+  Location.findOne({ id: p.id }, function(err, location){
+    errorHandler.qc(err, res, location);
+    res.json(200, location);
+  });
+}
+
+function modify(req, res) {
+  var p = req.params.all();
+
+  async.waterfall([ fetchLocation(), modifyLocation()], sendResponse());
+
+  function fetchLocation(callback) {
+    Location.findOne({ id: p.id }, function(err, location) {
+      callback(err, location);
+    });
+  }
+
+  function modifyLocation(location, callback) {
+    Location.update(location, p, function(err, location) {
+      callback(err, location);
+    });
+  }
+
+  function sendResponse(err, location) {
+    errorHandler.qc(err, res, location);
+    res.json(200, location);
+  }
+}
+
+function deactivate(req, res) {
+  var p = req.params.all();
+
+  async.waterfall([ fetchLocation(), deactivateLocation()], sendResponse());
+
+  function fetchLocation(callback) {
+    Location.findOne({ id: p.id }, function(err, location) {
+      callback(err, location);
+    });
+  }
+
+  function deactivateLocation(location, callback) {
+    Location.update(location, { active: false }, function(err, location) {
+      callback(location);
+    });
+  }
+
+  function sendResponse(err, location) {
+    errorHandler.qc(err, res, location);
+  }
+}
