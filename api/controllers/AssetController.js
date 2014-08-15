6d0d55111acf0e122b179cb9537e2c2661e96de2
Will Ricketts <will@willricketts.com>
2014-08-15 09:45:45 -0400

    refactored assetController

--- api/controllers/AssetController.js
+++ api/controllers/AssetController.js
@@ -6,59 +6,72 @@
  */
 
 module.exports = {
-
-  create: function(req, res) {
-    var p = req.params.all();
-
-    Asset.create(p, function(err, asset) {
-      errorHandler.qc(err, res, asset);
-      res.json(201, asset);
-    });
-  },
-
-  index: function(req, res) {
-    Asset.find()
-        .then(function(assets) {
-          errorHandler.nullCollection(assets, res);
-          res.json(200, assets);
-        }).fail(function(err) {
-          errorHandler.serverError(err, res);
-        });
-  },
-
-  show: function(req, res) {
-    var p = req.params.all();
-
-    Asset.findOne({ id: p.id }, function(err, asset) {
-      errorHandler.serverError(err, res);
-      errorHandler.nullCollection(asset, res);
-      res.status(200);
-      res.json(asset);
-    });
-  },
-
-  modify: function(req, res) {
-    var p = req.params.all();
-
-    Asset.update({ id: p.id }, p, function(err, asset) {
-      errorHandler.qc(err, res, asset);
-      res.json(200, asset);
-    });
-  },
-
-  deactivate: function(req, res) {
-    var p = req.params.all();
-
-    Asset.findOne({ id: p.id }, function(err, asset) {
-      errorHandler.serverError(err, res);
-      errorHandler.nullCollection(asset, res);
-      Asset.update(asset, { active: false, visible: false }, function(err, asset) {
-        errorHandler.serverError(err, res);
-        errorHandler.nullCollection(asset, res);
-        res.status(200);
-        res.json(asset);
-      });
-    });
-  }
+  create: create,
+  index: index,
+  show: show,
+  modify: modify,
+  deactivate: deactivate
 };
 
+function create(req, res) {
+  var p = req.params.all();
+
+  Asset.create(p, function(err, asset) {
+    errorHandler.qc(err, res, asset);
+    res.json(201, asset);
+  });
+}
+
+function index(req, res) {
+  Asset.find()
+      .then(function(assets) {
+        errorHandler.nullCollection(assets, res);
+        res.json(200, assets);
+      }).fail(function(err) {
+        errorHandler.serverError(err, res);
+      });
+}
+
+function show(req, res) {
+  var p = req.params.all();
+
+  Asset.findOne({ id: p.id }, function(err, asset) {
+    errorHandler.serverError(err, res);
+    errorHandler.nullCollection(asset, res);
+    res.status(200);
+    res.json(asset);
+  });
+}
+
+function modify(req, res) {
+  var p = req.params.all();
+
+  Asset.update({ id: p.id }, p, function(err, asset) {
+    errorHandler.qc(err, res, asset);
+    res.json(200, asset);
+  });
+}
+
+function deactivate(req, res) {
+  
+  var p = req.params.all();
+
+  async.waterfall([ fetchAsset(), deactivateAsset()], sendResponse());
+
+  function fetchAsset(callback) {
+    Asset.findOne({ id: p.id }, function(err, asset){
+      callback(err, asset);
+    });
+  }
+
+  function deactivateAsset(asset, callback) {
+    Asset.update(asset, { active: false, visible: false }, function(err, asset) {
+      callback(asset);
+    });
+  }
+
+  function sendResponse(err, asset) {
+    errorHandler.qc(err, res, asset);
+    res.json(200, asset);
+  }
+}
