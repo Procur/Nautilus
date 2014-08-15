f5a4a7e72e79a064d8d48b411e0168b36133fc10
Will Ricketts <will@willricketts.com>
2014-08-15 10:56:07 -0400

    added token generator service

--- api/services/tokenFunctions.js
+++ api/services/tokenFunctions.js
@@ -1,3 +1,16 @@
+var uuid = require('node-uuid');
+
 module.exports = {
 
+  generateToken: generateToken
+
 };
+
+function generateToken(callback) {
+  var token = uuid.v1();
+
+  ApiToken.findOne({ token: token }, function(err, token) {
+    errorHandler.serverError(err, res);
+    callback(token);
+  });
+}
