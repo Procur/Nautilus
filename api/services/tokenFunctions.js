40199fe789ff7a781ba262467a9d1386dc36ec4a
Will Ricketts <will@willricketts.com>
2014-08-18 13:31:05 -0400

    added replaceToken function that probably doesn't work

--- api/services/tokenFunctions.js
+++ api/services/tokenFunctions.js
@@ -4,7 +4,8 @@ module.exports = {
 
   generateToken: generateToken,
   checkUserForToken: checkUserForToken,
-  saveToken: saveToken
+  saveToken: saveToken,
+  replaceToken: replaceToken
 
 };
 
@@ -25,4 +26,48 @@ function saveToken(userId, token, callback) {
     errorHandler.qc(err, res, token);
     callback(token);
   });
+}
+
+function replaceToken(userId, token, callback) {
+
+  async.waterfall([
+    checkForOldToken,
+    destroyOldToken,
+    generateNewToken,
+    checkNewTokenUniqueness,
+    issueToUser
+  ], handleError);
+
+  function checkForOldToken(userId, token, callback) {
+    ApiToken.findOne({ user: userId, token: token }, function(err, token) {
+      callback(err, token);
+    });
+  }
+
+  function destroyOldToken(token, callback) {
+    ApiToken.destroy({ token: token }, function(err, token) {
+      callback(err, token);
+    })
+  }
+
+  function generateNewToken(callback) {
+    var newToken = uuid.v1();
+    callback(newToken);
+  }
+
+  function checkNewTokenUniqueness(newToken, callback) {
+    ApiToken.findOne({ token: newToken }, function(err, oldToken) {
+      callback(oldToken);
+    });
+  }
+
+  function issueToUser(userId, token, callback) {
+    ApiToken.create({ user: userId, token: token }, function (err, newToken) {
+      callback(newToken);
+    });
+  }
+
+  function handleError(err, newToken) {
+    errorHandler.qc(err, res, newToken);
+  }
 }
