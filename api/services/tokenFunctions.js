a16e02d9d518227cd029912cb16c200eaa9d63e2
Will Ricketts <will@willricketts.com>
2014-08-18 13:21:56 -0400

    adding token functions

--- api/services/tokenFunctions.js
+++ api/services/tokenFunctions.js
@@ -2,11 +2,27 @@ var uuid = require('node-uuid');
 
 module.exports = {
 
-  generateToken: generateToken
+  generateToken: generateToken,
+  checkUserForToken: checkUserForToken,
+  saveToken: saveToken
 
 };
 
 function generateToken(callback) {
   var token = uuid.v1();
   callback(token);
+}
+
+function checkUserForToken(userId, callback) {
+  ApiToken.findOne({ user: userId }, function(err, token) {
+    errorHandler.serverError(err, res);
+    callback(token);
+  });
+}
+
+function saveToken(userId, token, callback) {
+  ApiToken.create({ user: userId, token: token }, function(err, token) {
+    errorHandler.qc(err, res, token);
+    callback(token);
+  });
 }
