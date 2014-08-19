3c817ffa1f11c50eec6835712912c5c18ae978ef
Will Ricketts <will@willricketts.com>
2014-08-19 15:21:35 -0400

    added apitoken validation policy

--- api/policies/hasValidApiToken.js
+++ api/policies/hasValidApiToken.js
@@ -1,3 +1,15 @@
 module.exports = function(req, res, next) {
 
+  var apiToken = req.headers.apitoken;
+
+  ApiToken.findOne({ token: apiToken }, function(err, token) {
+    if(err) { return res.send(500); }
+    if(token !== undefined) {
+      return next();
+    }
+    else {
+      return res.send(400, 'Invalid API token');
+    }
+  });
+
 };
