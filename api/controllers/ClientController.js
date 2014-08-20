cf845918cc5f3e1fa086d0791f0b2d532c3fca5a
Will Ricketts <will@willricketts.com>
2014-08-20 10:45:10 -0400

    filled out controller actions

--- api/controllers/ClientController.js
+++ api/controllers/ClientController.js
@@ -15,17 +15,35 @@ module.exports = {
 };
 
 function create(req, res) {
+  var p = req.params.all();
 
+  Client.create(p, function(err, client) {
+    if(err){ return res.send(500); }
+    res.json(201, client);
+  });
 }
 
 function index(req, res) {
-
+  Client.find(function(err, clients) {
+    if(err){ return res.send(500); }
+    res.json(200, clients);
+  });
 }
 
 function modify(req, res) {
+  var p = req.params.all();
 
+  Client.update(p, function(err, client) {
+    if(err){ return res.send(500); }
+    res.json(200, client);
+  });
 }
 
 function destroy(req, res) {
+  var p = req.params.all();
 
+  Client.destroy(p, function(err){
+    if(err){ return res.send(500); }
+    res.send(200, 'Client removed');
+  });
 }
