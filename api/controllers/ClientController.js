1272503b613211cde17679996cf6b4fb1c075b21
Will Ricketts <will@willricketts.com>
2014-08-20 10:41:16 -0400

    added controller actions

--- api/controllers/ClientController.js
+++ api/controllers/ClientController.js
@@ -6,6 +6,26 @@
  */
 
 module.exports = {
-	
+
+  create: create,
+  index: index,
+  modify: modify,
+  destroy: destroy
+
 };
 
+function create(req, res) {
+
+}
+
+function index(req, res) {
+
+}
+
+function modify(req, res) {
+
+}
+
+function destroy(req, res) {
+
+}
