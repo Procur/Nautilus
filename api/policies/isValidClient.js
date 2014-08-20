66852f9b7adfe2577c77e447655650b4863ecdd6
Will Ricketts <will@willricketts.com>
2014-08-20 11:39:22 -0400

    added isValidClient policy

---
+++ api/policies/isValidClient.js
@@ -0,0 +1,17 @@
+module.exports = function(req, res, next) {
+
+  var clientId = req.headers.clientid,
+      host = req.host;
+
+  Client.findOne({ id: clientId }, function(err, client) {
+    if(err) { return res.send(500); }
+    if(client !== undefined) {
+      if (client.allowedHosts.indexOf(host) > -1) {
+        return next();
+      }
+    }
+    else {
+      return res.send(400, 'Invalid client ID');
+    }
+  });
+};
