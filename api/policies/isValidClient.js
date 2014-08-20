32a8a488d7e5e005424fc1982dd5708fd25278a4
Will Ricketts <will@willricketts.com>
2014-08-20 11:40:59 -0400

    fix

--- api/policies/isValidClient.js
+++ api/policies/isValidClient.js
@@ -6,7 +6,7 @@ module.exports = function(req, res, next) {
   Client.findOne({ id: clientId }, function(err, client) {
     if(err) { return res.send(500); }
     if(client !== undefined) {
-      if (client.allowedHosts.indexOf(host) > -1) {
+      if ((client.allowedHosts.indexOf(host) > -1) && (clientId == client.id)) {
         return next();
       }
     }
