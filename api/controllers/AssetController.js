fed84d4f503b1927b5c1fac6b8040276518867ab
Kyle Cline <kyledcline@gmail.com>
2014-08-22 14:09:10 -0400

    Asyncify AssetController and LocationController

--- api/controllers/AssetController.js
+++ api/controllers/AssetController.js
@@ -14,38 +14,24 @@ module.exports = {
 };
 
 function index(req, res) {
-  Asset.find().exec(sendResponse(req, res));
+  Asset.find().exec(Responder.dispatch(req, res));
 }
 
 function create(req, res) {
-  Asset.create(req.params.all()).exec(sendResponse(req, res, 201));
+  Asset.create(req.params.all()).exec(Responder.dispatch(req, res, 201));
 }
 
 function show(req, res) {
   var p = req.params.all();
-  Asset.findOne({ id: p.id }).exec(sendResponse(req, res));
+  Asset.findOne({ id: p.id }).exec(Responder.dispatch(req, res));
 }
 
 function modify(req, res) {
   var p = req.params.all();
-  Asset.update({ id: p.id }, p).exec(sendResponse(req, res, 201));
+  Asset.update({ id: p.id }, p).exec(Responder.dispatch(req, res, 201));
 }
 
 function deactivate(req, res) {
   var p = req.params.all();
-  Asset.deactivate(p.id).exec(sendResponse(req, res));
-}
-
-function sendResponse(req, res, successStatusCode) {
-  return function (err, object) {
-    successStatusCode = successStatusCode || 200;
-    var e = ErrorHandler.intercept(err, object, req.params.all());
-
-    if (e) { 
-      return res.json(e.status, e);
-    }
-    else {
-      return res.json(successStatusCode, object);
-    }
-  };
+  Asset.deactivate(p.id).exec(Responder.dispatch(req, res));
 }
