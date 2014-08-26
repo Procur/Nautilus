cbfa2b2b095f0d691d391930766e6272e696e7a4
Kyle Cline <kyledcline@gmail.com>
2014-08-26 11:06:10 -0400

    Change route and fix CompanyController.modify

--- config/routes.js
+++ config/routes.js
@@ -1,17 +1,18 @@
 module.exports.routes = {
 
-  'get /': 'UtilityController.heartbeat',
-  'get /docs': 'UtilityController.docs',
+  'get /'           : 'UtilityController.heartbeat',
+  'get /docs'       : 'UtilityController.docs',
 
-  'post /signup': 'UserController.create',
-  'post /login': 'AuthController.login',
-  'get /logout': 'AuthController.logout',
+  'post /signup'    : 'UserController.create',
+  'post /login'     : 'AuthController.login',
+  'get /logout'     : 'AuthController.logout',
 
-  'get /users': 'UserController.show',
-  'put /users': 'UserController.modify',
-  'delete /users': 'UserController.deactivate',
+  'get /users'      : 'UserController.show',
+  'put /users'      : 'UserController.modify',
+  'delete /users'   : 'UserController.deactivate',
 
-  'get /companies': 'CompanyController.index',
-  'post /company': 'CompanyController.create'
+  'get /companies'  : 'CompanyController.index',
+  'post /companies' : 'CompanyController.create',
+  'put /companies'  : 'CompanyController.modify'
 
 };
