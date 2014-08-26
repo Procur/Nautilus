453527a8a08d91cda544e6c3ae46d11b090c6b7b
Will Ricketts <will@willricketts.com>
2014-08-26 13:18:10 -0400

    fixed supplier controller

--- api/controllers/SupplierController.js
+++ api/controllers/SupplierController.js
@@ -7,55 +7,57 @@
 
 module.exports = {
 
-  create: function(req, res) {
-    var p = req.params.all(),
-        apiToken = req.headers.apitoken;
+  index: index,
+  create: create,
+  show: show,
+  modify: modify,
+  deactivate: deactivate
+};
 
-    userFunctions.findByApiToken(apiToken, function(user) {
-      if(!user.supplier) {
-        Supplier.create(p, function(err, supplier) {
-          if(err) { return res.send(500); }
-          return res.json(201, supplier); //TODO: I feel like this needs to update company as well.
-        });
+function index(req, res) {
+  Supplier.find().exec(function(err, suppliers) {
+    if(err){ res.send(500) }
+    res.json(200, suppliers);
+  });
+}
+
+function create(req, res) {
+  var p = req.params.all();
+
+  async.waterfall([ rejectIfAlreadySupplier, createSupplier, updateUser ], Responder.dispatch(req, res, 201));
+
+  function rejectIfAlreadySupplier(cb) {
+    Supplier.findOne({ id: req.currentUser.supplier }, function(err, supplier) {
+      if(supplier === undefined){
+        cb(err, supplier);
       }
       else {
-        res.send(400, 'This company is already a supplier.')
+        res.send(400, 'User is already associated with a supplier');
       }
-    });
-  },
-
-  index: function(req, res) {
-    Supplier.find(function(err, suppliers) {
-      if(err) { return res.send(500); }
-      res.json(200, suppliers);
-    });
-  },
-
-  show: function(req, res) {
-    var p = req.params.all();
-
-    Supplier.findOne({ id: p.id }, function(err, supplier) {
-      if(err) { return res.send(500); }
-      res.json(200, supplier);
-    });
-  },
-
-  modify: function(req, res) {
-    var p = req.params.all();
-
-    Supplier.update(p, function(err, supplier) {
-      if(err) { return res.send(500); }
-      res.json(200, supplier);
-    });
-  },
-
-  deactivate: function(req, res) {
-    var p = req.params.all();
-
-    Supplier.update({ active: false }, function(err, supplier) {
-      if(err) { return res.send(500); }
-      res.send(200, 'Supplier has been deactivated.');
     });
   }
 
-};
+  function createSupplier(buyer, cb) {
+    Supplier.create(p, function(err, supplier) {
+      cb(err, supplier);
+    });
+  }
+
+  function updateUser(supplier, cb) {
+    Supplier.update(req.currentUser.id, { supplier: supplier.id }, function(err, user) {
+      cb(err, user);
+    });
+  }
+}
+
+function show(req, res) {
+  Supplier.findOne(req.currentUser.supplier, Responder.dispatch(req, res));
+}
+
+function modify(req, res) {
+  Supplier.update(req.currentUser.supplier, p, Responder.dispatch(req, res, 201));
+}
+
+function deactivate(req, res) {
+  Supplier.update(req.currentUser.supplier, { active: false }, Responder.dispatch(req, res, 201));
+}
