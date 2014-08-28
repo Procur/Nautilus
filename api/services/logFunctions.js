33c3d772b1f0c0b2ef7bc50c882404215c72657e
Will Ricketts <will@willricketts.com>
2014-08-28 13:12:34 -0400

    added payload to invalidHost

--- api/services/logFunctions.js
+++ api/services/logFunctions.js
@@ -25,12 +25,16 @@ function badApiToken(req, callback) {
 function invalidHost(req, callback) {
 
   var payload = {
-
+    ip: req.ip,
+    host: req.host,
+    params: req.params.all(),
+    headers: req.headers,
+    path: req.path,
+    apitoken: req.headers.apitoken
   };
 
   Log.create(payload, function(err, log) {
     if(err) { return res.send(500); }
     callback(log);
   });
-
 }
